3134cfdb686c0c36a2f868342f4a357d80d5034c
CarlosHdzR <hernandezrodriguez@uninorte.edu.co>
2022-06-29 14:18:10 -0500

    Agregando logos

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,26 +3,29 @@ import './NavBar.css';
 import { Link } from 'react-scroll';
 import ToggleBtn from "./ToggleBtn";
 
-const navItems = [
-    {
-        id: "1",
-        label: "About",
-        to: "about",
-    },
-    {
-        id: "2",
-        label: "Projects",
-        to: "projects",
-    },
-    {
-        id: "3",
-        label: "Contact",
-        to: "contact",
-    }
-]
 
 const NavBar = ({ isScrolling }) => {
     const [showLinks, setShowLinks] = useState(false)
+    
+    const navItems = [
+        {
+            id: "1",
+            label: "About",
+            to: "about",
+        },
+        {
+            id: "2",
+            label: "Projects",
+            to: "projects",
+        },
+        {
+            id: "3",
+            label: "Contact",
+            to: "contact",
+        }
+    ]
+
+    const logo = "https://res.cloudinary.com/chrod90/image/upload/v1656525814/chrod/logo-chrod-2_mt3x7e.png"
 
     return (
         <nav className={`navbar navbar-expand ${isScrolling > 300 ? "scrolling" : null}`}>
@@ -35,7 +38,8 @@ const NavBar = ({ isScrolling }) => {
                     offset={0}
                     duration={200}
                 >
-                    CarlosHRod <i className="fa-solid fa-terminal fa-fade" />
+                    <img src={logo} alt="chrod-logo"/>
+                    <i className="fa-solid fa-terminal fa-fade" />
                 </Link>
                 <ul className="navbar-nav active" id={showLinks ? "hidden" : ""}>
                     {navItems.map((item) => (
