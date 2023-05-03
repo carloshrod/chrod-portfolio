5d5330d775beb7cebfcd61a588a351c177762a49
chrod <chrod.dev@gmail.com>
2023-05-03 16:44:16 -0500

    Update cover subtitle animation

--- src/components/Cover.jsx
+++ src/components/Cover.jsx
@@ -1,24 +1,24 @@
 import coverVideo from '../assets/media/cover-video.mp4';
 import { useLanguageContext } from '../hooks';
-import { motion } from "framer-motion";
+import { motion } from 'framer-motion';
 
 const Cover = () => {
-    const { texts } = useLanguageContext()
+	const { language, texts } = useLanguageContext();
 
-    return (
-        <div className="cover-container" id="hero">
-            <video className="video" src={coverVideo} autoPlay loop muted />
-            <motion.h1
-                className="mt-5"
-                initial={{ opacity: 0, scale: 0.5 }}
-                animate={{ opacity: 1, scale: 1 }}
-                transition={{ type: "spring", stiffness: 150 }}
-            >
-                Carlos Hernández R.
-            </motion.h1>
-            <span>{texts.coverSubtitle}</span>
-        </div>
-    )
-}
+	return (
+		<div className='cover-container' id='hero'>
+			<video className='video' src={coverVideo} autoPlay loop muted />
+			<motion.h1
+				className='mt-5'
+				initial={{ opacity: 0, scale: 0.5 }}
+				animate={{ opacity: 1, scale: 1 }}
+				transition={{ type: 'spring', stiffness: 150 }}
+			>
+				Carlos Hernández R.
+			</motion.h1>
+			<span className={language}>{texts.coverSubtitle}</span>
+		</div>
+	);
+};
 
 export default Cover;
