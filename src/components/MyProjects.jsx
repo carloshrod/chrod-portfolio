09e6907a09aec6d96d81a1c2c9baf6c9d0b6fb09
chrod <chrod.dev@gmail.com>
2023-06-23 18:06:11 -0500

    Update project slides

--- src/components/MyProjects.jsx
+++ src/components/MyProjects.jsx
@@ -1,63 +1,62 @@
-import Slides from "./Slides";
-import { useLanguageContext } from "../hooks";
+import Slides from './Slides';
+import { useLanguageContext } from '../hooks';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Keyboard, Autoplay, EffectCube } from 'swiper';
+import { Navigation, Keyboard, Autoplay, EffectFlip } from 'swiper';
 import 'swiper/scss';
 import 'swiper/scss/navigation';
 import 'swiper/scss/pagination';
-import 'swiper/scss/effect-cube';
-import { motion } from "framer-motion";
+import 'swiper/scss/effect-flip';
+import { motion } from 'framer-motion';
 
 const MyProjects = () => {
-    const { texts } = useLanguageContext()
+	const { texts } = useLanguageContext();
 
-    return (
-        <div className="my-projects-container" id="projects">
-            <motion.div
-                className="my-projects-title"
-                initial={{ opacity: 0, scale: 0 }}
-                whileInView={{ opacity: 1, scale: 1 }}
-                transition={{ type: "spring", stiffness: 50 }}
-                viewport={{ once: true }}
-            >
-                <h2
-                >{texts.projectsLabel}</h2>
-                <i className='bx bx-like bx-sm bx-tada' />
-            </motion.div>
-            <motion.div
-                className="swiperContainer"
-                initial={{ opacity: 0, scale: 0 }}
-                whileInView={{ opacity: 1, scale: 1 }}
-                transition={{ delay: .3, type: "spring", stiffness: 50 }}
-                viewport={{ once: true }}
-            >
-                <Swiper
-                    modules={[Navigation, Keyboard, Autoplay, EffectCube]}
-                    spaceBetween={20}
-                    slidesPerView={1}
-                    style={{
-                        "--swiper-navigation-color": "#fff",
-                        "--swiper-navigation-size": "1.5rem",
-                        "--swiper-pagination-color": "#fff",
-                    }}
-                    navigation
-                    keyboard={{
-                        enabled: true,
-                    }}
-                    autoplay={{
-                        delay: 5000,
-                        disableOnInteraction: false,
-                    }}
-                    loop={true}
-                    effect="cube"
-                >
-                    {Slides.map((slide, i) => (
-                        <SwiperSlide key={i}>{slide}</SwiperSlide>
-                    ))}
-                </Swiper>
-            </motion.div>
-        </div>
-    );
+	return (
+		<div className='my-projects-container' id='projects'>
+			<motion.div
+				className='my-projects-title'
+				initial={{ opacity: 0, scale: 0 }}
+				whileInView={{ opacity: 1, scale: 1 }}
+				transition={{ type: 'spring', stiffness: 50 }}
+				viewport={{ once: true }}
+			>
+				<h2>{texts.projectsLabel}</h2>
+				<i className='bx bx-like bx-sm bx-tada' />
+			</motion.div>
+			<motion.div
+				className='swiperContainer'
+				initial={{ opacity: 0, scale: 0 }}
+				whileInView={{ opacity: 1, scale: 1 }}
+				transition={{ delay: 0.3, type: 'spring', stiffness: 50 }}
+				viewport={{ once: true }}
+			>
+				<Swiper
+					modules={[Navigation, Keyboard, Autoplay, EffectFlip]}
+					spaceBetween={20}
+					slidesPerView={1}
+					style={{
+						'--swiper-navigation-color': '#fff',
+						'--swiper-navigation-size': '1.5rem',
+						'--swiper-pagination-color': '#fff',
+					}}
+					navigation
+					keyboard={{
+						enabled: true,
+					}}
+					// autoplay={{
+					// 	delay: 5000,
+					// 	disableOnInteraction: false,
+					// }}
+					loop={true}
+					effect='flip'
+				>
+					{Slides.map((slide, i) => (
+						<SwiperSlide key={i}>{slide}</SwiperSlide>
+					))}
+				</Swiper>
+			</motion.div>
+		</div>
+	);
 };
 
 export default MyProjects;
--- src/utils/projectsInfo.js
+++ src/utils/projectsInfo.js
@@ -1,52 +1,49 @@
 export const projectsInfo = [
-    {
-        src: "https://res.cloudinary.com/chrod90/image/upload/v1674326685/project-captures/screenshot-predial_rml54n.jpg",
-        title: "MERN Stack App (CRUD, Auth): ",
-        description: "Hooks, Context API, Reducers, React-Router, Bootstrap, Fetch API, JWT-decode, Bcryptjs, Chart.js, Node, Express, MongoDb, Mongoose, Cloudinary, Jsonwebtoken, Nodemailer.",
-        link1: {
-            label: "Frontend",
-            url: "https://github.com/carloshrod/predial-frontend"
-        },
-        link2: {
-            label: "Deploy",
-            url: "https://gestion-catastral.netlify.app",
-            icon: "bx bxl-netlify bx-md bx-tada-hover"
-        },
-        link3: {
-            label: "Backend",
-            url: "https://github.com/carloshrod/predial-backend"
-        }
-    },
-    {
-        src: "https://res.cloudinary.com/chrod90/image/upload/v1674237674/project-captures/screenshot-movies-app_vfvwni.jpg",
-        title: "PERN Stack App (CRUD): ",
-        description: "Vite, Hooks, Context API, React-Router, SCSS, Axios, Node, Express, PostgreSQL, Node-Postgres, Cloudinary.",
-        link1: {
-            label: "Frontend",
-            url: "https://github.com/carloshrod/movies-frontend",
-        },
-        link2: {
-            label: "Deploy",
-            url: "https://chrod-movies.netlify.app/",
-            icon: "bx bxl-netlify bx-md bx-tada-hover"
-        },
-        link3: {
-            label: "Backend",
-            url: "https://github.com/carloshrod/movies-backend"
-        }
-    },
-    {
-        src: "https://res.cloudinary.com/chrod90/image/upload/v1674769783/project-captures/screenshot-crypto-app_clszxf.jpg",
-        title: "React App - CoinGecko API: ",
-        description: "Vite, Hooks, Context API, SCSS, Bootstrap, Fetch API, React-Spinners.",
-        link1: {
-            label: "Frontend",
-            url: "https://github.com/carloshrod/chrod-crypto-app",
-        },
-        link2: {
-            label: "Deploy",
-            url: "https://chrod-crypto.vercel.app/",
-            icon: "bi bi-triangle"
-        }
-    }
+	{
+		src: 'https://res.cloudinary.com/chrod90/image/upload/v1674326685/project-captures/screenshot-predial_rml54n.jpg',
+		title: 'MERN Stack App (CRUD, Auth)',
+		link1: {
+			label: 'Frontend',
+			url: 'https://github.com/carloshrod/predial-frontend',
+		},
+		link2: {
+			label: 'Deploy',
+			url: 'https://gestion-catastral.netlify.app',
+			icon: 'bx bxl-netlify bx-md bx-tada-hover',
+		},
+		link3: {
+			label: 'Backend',
+			url: 'https://github.com/carloshrod/predial-backend',
+		},
+	},
+	{
+		src: 'https://res.cloudinary.com/chrod90/image/upload/v1674237674/project-captures/screenshot-movies-app_vfvwni.jpg',
+		title: 'PERN Stack App (CRUD)',
+		link1: {
+			label: 'Frontend',
+			url: 'https://github.com/carloshrod/movies-frontend',
+		},
+		link2: {
+			label: 'Deploy',
+			url: 'https://chrod-movies.netlify.app/',
+			icon: 'bx bxl-netlify bx-md bx-tada-hover',
+		},
+		link3: {
+			label: 'Backend',
+			url: 'https://github.com/carloshrod/movies-backend-ts',
+		},
+	},
+	{
+		src: 'https://res.cloudinary.com/chrod90/image/upload/v1674769783/project-captures/screenshot-crypto-app_clszxf.jpg',
+		title: 'React App - CoinGecko API',
+		link1: {
+			label: 'Frontend',
+			url: 'https://github.com/carloshrod/chrod-crypto-app',
+		},
+		link2: {
+			label: 'Deploy',
+			url: 'https://chrod-crypto.vercel.app/',
+			icon: 'bi bi-triangle',
+		},
+	},
 ];
