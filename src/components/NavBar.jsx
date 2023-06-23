04af1bcbe3562d83f4ec2de4bac11d737306aec1
chrod <chrod.dev@gmail.com>
2023-06-23 18:09:00 -0500

    Fix navbar styles

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,73 +1,73 @@
-import { useState } from "react";
+import { useState } from 'react';
 import { Link } from 'react-scroll';
-import { useLanguageContext } from "../hooks";
-import ToggleBtn from "./ToggleBtn";
-import ToggleLanguage from "./ToggleLanguage";
+import { useLanguageContext } from '../hooks';
+import ToggleBtn from './ToggleBtn';
+import ToggleLanguage from './ToggleLanguage';
 import logo from '../assets/media/chrod-logo.png';
-import { motion } from "framer-motion";
+import { motion } from 'framer-motion';
 
-const NavBar = ({ isScrolling }) => {
-    const { texts } = useLanguageContext();
-    const [showLinks, setShowLinks] = useState(false);
+const NavBar = ({ scrollHeight }) => {
+	const { texts } = useLanguageContext();
+	const [showLinks, setShowLinks] = useState(false);
 
-    const navItems = [
-        {
-            label: texts.aboutLabel,
-            to: "about",
-        },
-        {
-            label: texts.projectsLabel,
-            to: "projects",
-        },
-        {
-            label: texts.contactLabel,
-            to: "contact",
-        }
-    ]
+	const navItems = [
+		{
+			label: texts.aboutLabel,
+			to: 'about',
+		},
+		{
+			label: texts.projectsLabel,
+			to: 'projects',
+		},
+		{
+			label: texts.contactLabel,
+			to: 'contact',
+		},
+	];
 
-
-    return (
-        <motion.nav
-            className={`navbar navbar-expand ${isScrolling > 300 ? "scrolling" : null}`}
-            initial={{ y: "-100%" }}
-            animate={{ y: "0%" }}
-            transition={{ type: "spring", stiffness: 150 }}
-        >
-            <div
-                className="container-fluid"
-            >
-                <Link
-                    className="navbar-brand"
-                    to={"hero"}
-                    spy={true}
-                    smooth={true}
-                    offset={0}
-                    duration={200}
-                >
-                    <img src={logo} alt="chrod-logo" />
-                    <i className="fa-solid fa-terminal fa-fade" />
-                </Link>
-                <ul className="navbar-nav s-auto" id={showLinks ? "hidden" : ""}>
-                    {navItems.map((item, i) => (
-                        <li key={i} className="nav-item">
-                            <Link
-                                className="nav-link"
-                                to={item.to} spy={true}
-                                smooth={true}
-                                offset={-80}
-                                duration={200}
-                                onClick={() => setShowLinks(false)}
-                            >
-                                {item.label}
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
-                <ToggleLanguage />
-                <ToggleBtn showLinks={showLinks} setShowLinks={setShowLinks} />
-            </div>
-        </motion.nav>
-    )
-}
+	return (
+		<motion.nav
+			className={`navbar navbar-expand ${
+				scrollHeight > 545 ? 'scrolling' : null
+			}`}
+			initial={{ y: '-100%' }}
+			animate={{ y: '0%' }}
+			transition={{ type: 'spring', stiffness: 150 }}
+		>
+			<div className='container-fluid'>
+				<Link
+					className='navbar-brand'
+					to={'hero'}
+					spy={true}
+					smooth={true}
+					offset={0}
+					duration={200}
+				>
+					<img src={logo} alt='chrod-logo' />
+					<i className='fa-solid fa-terminal fa-fade' />
+				</Link>
+				<ul className='navbar-nav s-auto' id={showLinks ? 'hidden' : ''}>
+					{navItems.map((item, i) => (
+						<li key={i} className='nav-item'>
+							<Link
+								className='nav-link'
+								to={item.to}
+								spy={true}
+								smooth={true}
+								offset={-80}
+								duration={200}
+								onClick={() => setShowLinks(false)}
+							>
+								{item.label}
+							</Link>
+						</li>
+					))}
+				</ul>
+				<ToggleLanguage />
+				<ToggleBtn showLinks={showLinks} setShowLinks={setShowLinks} />
+			</div>
+		</motion.nav>
+	);
+};
 
 export default NavBar;
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,46 +1,52 @@
 import { useEffect, useState } from 'react';
-import { Cover, About, MyProjects, Contact, Footer, Navbar } from '../components';
+import {
+	Cover,
+	About,
+	MyProjects,
+	Contact,
+	Footer,
+	Navbar,
+} from '../components';
 import { LanguageProvider } from '../context/LanguageContext';
-import PacmanLoader from "react-spinners/PacmanLoader";
+import PacmanLoader from 'react-spinners/PacmanLoader';
 
 function Portfolio() {
-    const [scrollHeight, setScrollHeight] = useState(0);
-    const [isLoading, setIsLoading] = useState(true);
+	const [scrollHeight, setScrollHeight] = useState(0);
+	const [isLoading, setIsLoading] = useState(true);
 
-    const handleScroll = () => {
-        const position = window.pageYOffset;
-        setScrollHeight(position);
-    };
+	const handleScroll = () => {
+		const position = window.pageYOffset;
+		setScrollHeight(position);
+	};
 
-    useEffect(() => {
-        window.addEventListener("scroll", handleScroll);
-    }, [scrollHeight]);
+	useEffect(() => {
+		window.addEventListener('scroll', handleScroll);
+	}, [scrollHeight]);
 
-    useEffect(() => {
-        setTimeout(() => {
-            setIsLoading(false);
-        }, 1500);
-    }, [])
+	useEffect(() => {
+		setTimeout(() => {
+			setIsLoading(false);
+		}, 1500);
+	}, []);
 
-    return (
-        <LanguageProvider>
-            {isLoading
-                ?
-                <div className="d-flex justify-content-center align-items-center vh-100">
-                    <PacmanLoader color="white" />
-                </div>
-                :
-                <>
-                    <Navbar isScrolling={scrollHeight} />
-                    <Cover />
-                    <About />
-                    <MyProjects />
-                    <Contact />
-                    <Footer />
-                </>
-            }
-        </LanguageProvider>
-    );
+	return (
+		<LanguageProvider>
+			{isLoading ? (
+				<div className='d-flex justify-content-center align-items-center vh-100'>
+					<PacmanLoader color='white' />
+				</div>
+			) : (
+				<>
+					<Navbar scrollHeight={scrollHeight} />
+					<Cover />
+					<About />
+					<MyProjects />
+					<Contact />
+					<Footer />
+				</>
+			)}
+		</LanguageProvider>
+	);
 }
 
 export default Portfolio;
